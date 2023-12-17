5d6f6eea0679084cae943795457406fd24df9744
Sarthak Kelkar <kelkarsarthak2002@gmail.com>
2023-12-18 01:21:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000; // or any port you prefer
+
+// Serve static files (CSS, images, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/about.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/contact.html'));
+});
+
+app.get('/search', (req, res) => {
+  // Handle search logic here and send the appropriate response
+  const query = req.query.query;
+  // Perform search logic using the query parameter
+  // For now, let's just send a simple response
+  res.send(`Search results for: ${query}`);
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
