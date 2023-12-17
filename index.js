1a08712d78537ff0affdce57f556ddc6a36e2c1b
Sarthak Kelkar <kelkarsarthak2002@gmail.com>
2023-12-18 01:58:50 +0530

    path change 3

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Define routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/about.html'));
+  res.sendFile(path.join(__dirname, 'about.html'));
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/contact.html'));
+  res.sendFile(path.join(__dirname, 'contact.html'));
 });
 
 app.get('/search', (req, res) => {
